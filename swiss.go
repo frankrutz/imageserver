165306011d4bf0b7f39a1c25cb9d2b9fe832395e
Frank Potthast Rutz <frank.potthast@gmail.com>
2018-09-09 16:37:06 +0200

    Update swiss.go

--- swiss.go
+++ swiss.go
@@ -7,13 +7,25 @@ import (
     "os/exec"
     "strconv"
     "unicode/utf8"
+    "unicode"
+    "strings"
 )
+
+func isLetter(s string) bool {
+    for _, r := range s {
+        if !unicode.IsLetter(r) {
+            return false
+        }
+    }
+    return true
+}
+
 func makeimage(vorname string) int {
     var rc int = 1
     fmt.Println("Hallo "+vorname);
     var imglength int
     inputname := vorname
-    printtext := inputname+"s"
+    printtext := inputname 
     outtext   := printtext+ "Life"
     outtextlength := utf8.RuneCountInString(outtext)
     imglength = 74 * outtextlength
@@ -22,6 +34,7 @@ func makeimage(vorname string) int {
     log.Println("log")
     return rc
 }
+
 func hello(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.Error(w, "hello: 404 not found.", http.StatusNotFound)
@@ -36,14 +49,25 @@ func hello(w http.ResponseWriter, r *http.Request) {
             fmt.Fprintf(w, "ParseForm() err: %v", err)
             return
         }
-        name := r.FormValue("name")
-        makeimage(name);
-        fmt.Fprintf(w,"<!DOCTYPE html><HTML>")
-        fmt.Fprintf(w,"<CENTER><a href=\"http://35.242.198.150/tmp/%s.png\">click to download %s.png</n></CENTER></HTML>",name,name)
+        name := strings.TrimSpace(r.FormValue("name"))
+        if name[len(name)-1:] == "s"{
+           name=name+""
+        } else {
+           name=name+"s"
+        }
+        if isLetter(name) {
+           makeimage(name)
+           fmt.Fprintf(w,"<!DOCTYPE html><HTML>")
+           fmt.Fprintf(w,"<CENTER><a href=\"http://35.242.198.150/tmp/%s.png\">click to download %s.png</n></CENTER></HTML>",name,name)
+           fmt.Fprintf(w,"<meta http-equiv=\"refresh\" content=\"1; url=./tmp/%s.png\" />",name)
+        } else {  
+           fmt.Fprintf(w,"invalid input.\nNo spaces.\nNo spacial characters.\nPlease go back and try again\n")  
+        }
     default:
         fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
     }
 }
+
 func main() {
     http.HandleFunc("/", hello)
     http.Handle("/tmp/", http.StripPrefix("/tmp/", http.FileServer(http.Dir("./tmp/"))))
